15b7d340fa9fdab4593d9dea1a9ba3998b9aa627
duhlite <travismingalone@gmail.com>
2019-08-07 09:31:59 -0600

    Added footer + footer components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TermsComponent } from './footer/terms/terms.component';
+import { PrivacyComponent } from './footer/privacy/privacy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    TermsComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
